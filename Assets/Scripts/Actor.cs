3e58cd71275b913ee5f03928a47bde82aaa42899
Rob Hailman <robhailman@gmail.com>
2015-04-18 01:29:44 -0400

    Start of NPCs
    Add: Civilian with animations
    Change: Move scripts into scripts folder
    Change: Mode common code into actor class

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour
+{
+
+	public string WalkN = "WalkN";
+	public string WalkE = "WalkE";
+	public string WalkS = "WalkS";
+	public string WalkW = "WalkW";
+	
+	public float MoveSpeed = 50f;
+	public float AnimationSpeedFactor = 0.03f;
+	
+	public Direction Direction;
+	public bool Moving;
+	
+	protected Animator animator;
+	protected Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	protected void UpdateAnimation (float speed)
+	{
+		// update sprite
+		switch (Direction) {
+		case Direction.NORTH:
+			animator.Play (WalkN);
+			break;
+		case Direction.EAST:
+			animator.Play (WalkE);
+			break;
+		case Direction.SOUTH:
+			animator.Play (WalkS);
+			break;
+		case Direction.WEST:
+			animator.Play (WalkW);
+			break;
+		}
+
+		animator.speed = speed;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Actor
 {
-	public int WalkNHash = Animator.StringToHash ("WalkN");
-	public int WalkEHash = Animator.StringToHash ("WalkE");
-	public int WalkSHash = Animator.StringToHash ("WalkS");
-	public int WalkWHash = Animator.StringToHash ("WalkW");
 
-	public float MoveSpeed = 50f;
-	public float AnimationSpeedFactor = 0.25f;
-
-	public Direction Direction;
-	public bool Moving;
-
-	private Animator animator;
-	private Rigidbody2D rigidBody;
 	// Use this for initialization
 	void Start ()
 	{
@@ -51,22 +39,6 @@ public class PlayerController : MonoBehaviour
 			Moving = true;
 		}
 
-		// update sprite
-		switch (Direction) {
-		case Direction.NORTH:
-			animator.Play (WalkNHash);
-			break;
-		case Direction.EAST:
-			animator.Play (WalkEHash);
-			break;
-		case Direction.SOUTH:
-			animator.Play (WalkSHash);
-			break;
-		case Direction.WEST:
-			animator.Play (WalkWHash);
-			break;
-		}
-
 		// move player
 		if (!Moving) {
 			moveVector *= 0;
@@ -74,7 +46,8 @@ public class PlayerController : MonoBehaviour
 			moveVector = moveVector.normalized * MoveSpeed;
 		}
 
-		animator.speed = moveVector.magnitude * AnimationSpeedFactor;
+		UpdateAnimation (moveVector.magnitude * AnimationSpeedFactor);
+
 		rigidBody.velocity = moveVector;
 	}
 }
